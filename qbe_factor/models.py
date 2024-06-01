8397afbca61790cf6905421f32d22f7c3c816310
Oran Looney <olooney@gmail.com>
2024-06-01 08:42:16 -0500

    load model data from package data

--- qbe_factor/models.py
+++ qbe_factor/models.py
@@ -1,4 +1,6 @@
 import json
+import pkgutil
+data = pkgutil.get_data('hermes', 'templates/python.tpl')
 
 class FactorModel:
     """
@@ -9,9 +11,18 @@ class FactorModel:
         self.data = factor_data
 
     @classmethod
-    def load(cls, filename):
-        with open(filename) as file:
-            factor_data = json.load(file)
+    def load(cls, filename=None):
+        """
+        Load the model's data from the Python package's data by default,
+        or allow the user to specify a different JSON file.
+        """
+        if filename is None:
+            raw_data = pkgutil.get_data('qbe_factor', 'data/data.json')
+            factor_data = json.loads(raw_data.decode('utf-8'))
+        else:
+            with open(filename) as file:
+                factor_data = json.load(file)
+
         return cls(factor_data)
         
 
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,8 +1,20 @@
 import pytest
 from qbe_factor.models import FactorModel
 
-def test_load():
-    model = FactorModel.load("data/data.json")
+
+def test_load_filename():
+    model = FactorModel.load("tests/test_data.json")
+    data = model.data['data']
+    for i in range(2):
+        assert data[i]['var_name'] == 'variable'
+        assert data[i]['category'] == f'value{i}'
+
+def test_load_default():
+    model = FactorModel.load()
+    data = model.data['data']
+    for i in range(len(data)):
+        assert 'var_name' in data[i]
+        assert 'category' in data[i]
 
 
 
